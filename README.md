ae0cefaa377fef5a05e56d1f3dbc0550df4da468
Soumwadeep Guha <soumwadeep@gmail.com>
2023-04-11 17:29:00 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # SafeNet
-Browser Extension Which Will Safe You From Hackers
-
-AIzaSyDUlKo3pK2TkjCnYIqE9THre-QQJbJAlx0
+Browser Extension Which Will Safe You From Hackers!
--- popup.js
+++ popup.js
@@ -1,10 +1,2 @@
-// popup.js
-
-document.addEventListener("DOMContentLoaded", function () {
-  var blockButton = document.getElementById("block-button");
-  blockButton.addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.update(tabs[0].id, { url: "https://www.google.com/" });
-    });
-  });
-});
+console.log('Its Working!')
+console.log('Hi User!')
